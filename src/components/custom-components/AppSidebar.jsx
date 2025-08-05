954e045fb20ca9e9611ba2a3f3973efbc2c84443
Johan Philip <johanbp27@gmail.com>
2025-08-05 16:28:58 -0400

    sidebar, nav bar and app header added

---
+++ src/components/custom-components/AppSidebar.jsx
@@ -0,0 +1,71 @@
+import React from 'react';
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarGroupLabel,
+  SidebarHeader,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from '../ui/sidebar';
+import blackLogo from '../../assets/black-logo.svg';
+import { CirclePlus, History, LayoutDashboard, LogOut } from 'lucide-react';
+
+export default function AppSidebar() {
+  return (
+    <Sidebar
+      className="border-secure-blue pt-16"
+      collapsible="icon"
+      variant="sidebar"
+    >
+      <SidebarContent>
+        <SidebarGroup>
+          <SidebarGroupLabel>Core</SidebarGroupLabel>
+          <SidebarGroupContent>
+            <SidebarMenu>
+              <SidebarMenuItem key={'Dashboard'}>
+                <SidebarMenuButton asChild>
+                  <a href={'/dashboard'}>
+                    <LayoutDashboard className="stroke-primary" />
+                    <span className="text-primary">Dashboard</span>
+                  </a>
+                </SidebarMenuButton>
+              </SidebarMenuItem>
+              <SidebarMenuItem key={'Upload Code'}>
+                <SidebarMenuButton
+                  asChild
+                  className="bg-secure-orange duration-500 ease-in-out hover:bg-secure-blue hover:dark:text-white"
+                >
+                  <a href={'/upload-code'}>
+                    <CirclePlus className="stroke-white" />
+                    <span className="text-white">Upload Code</span>
+                  </a>
+                </SidebarMenuButton>
+              </SidebarMenuItem>
+              <SidebarMenuItem key={'Upload History'}>
+                <SidebarMenuButton asChild>
+                  <a href={'/upload-history'}>
+                    <History className="stroke-black" />
+                    <span className="text-primary">Upload History</span>
+                  </a>
+                </SidebarMenuButton>
+              </SidebarMenuItem>
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
+      </SidebarContent>
+      <SidebarFooter>
+        <SidebarMenu>
+          <SidebarMenuItem>
+            <SidebarMenuButton>
+              <LogOut /> Log out
+            </SidebarMenuButton>
+          </SidebarMenuItem>
+        </SidebarMenu>
+      </SidebarFooter>
+    </Sidebar>
+  );
+}
